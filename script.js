b76320d1c2bd900135a459fbb8cf0707b10a3625
Satish Kr <satishkrgupta555@gmail.com>
2023-04-10 07:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ window.person = person;
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
+
+let team = players;
+let team1 = {};
+for(let key in players){
+	team1[key] = players[key];
+}
+let cap1 = {};
+for(let key in person){
+	cap1[key] = person[key];
+}
